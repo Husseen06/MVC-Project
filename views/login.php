5301c75c143cd1f1e7bc2e03c5efb8b79a2d463f
iShaakir <9022348@student.zadkine.nl>
2025-01-14 09:56:42 +0100

    Login gemaakt

--- views/login.php
+++ views/login.php
@@ -4,24 +4,13 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css">
-  <title>Registreren</title>
+  <link rel="stylesheet" href="css/styles.css">
+  <title>Login</title>
 </head>
-
 <body>
   <div class="box">
-    <h1>Registratie</h1>
+    <h1>Login</h1>
     <form method="POST" action="../controllers/UserController.php">
-    <div class="txt_field">
-        <input type="text" required id="naam" name="naam" />
-        <span></span>
-        <label for="naam">Naam</label>
-      </div>
-      <div class="txt_field">
-        <input type="text" required id="achternaam" name="achternaam" />
-        <span></span>
-        <label for="achternaam">Achternaam</label>
-      </div>
       <div class="txt_field">
         <input type="email" required id="email" name="email" />
         <span></span>
@@ -37,12 +26,11 @@
         <span></span>
         <label for="wachtwoord">Wachtwoord</label>
       </div>
-      <input type="submit" value="Registreren" name="register">
+      <input type="submit" value="Inloggen" name="inloggen">
       <div class="signup_link">
-        Al een account? <a href="login.php">Inloggen</a>
+       Nog geen account? <a href="register.php">registreren</a>
       </div>
     </form>
   </div>
 </body>
-
 </html>
