8b4d4826d37bc7d587eb5eab7a026da07f559a86
Thomas <9022371@student.zadkine.nl>
2025-01-20 13:29:44 +0100

    Formatie

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-include './views/register.html';
+require_once './controllers/UserController.php';
+require_once './controllers/FormationController.php';
+
+// Basic routing example
+$page = $_GET['page'] ?? 'login'; // Default to 'login'
+
+switch ($page) {
+    case 'login':
+        $controller = new UserController();
+        $controller->showLogin();
+        break;
+    case 'register':
+        $controller = new UserController();
+        $controller->showRegister();
+        break;
+    case 'formation':
+        $controller = new FormationController();
+        $controller->showFormation();  // Toon formatiepagina
+        break;
+    default:
+        echo "404 Page Not Found";
+        break;
+}
 ?>
